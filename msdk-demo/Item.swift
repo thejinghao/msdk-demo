d87bc12a09f14e2316acd86a3384c8326597186f
Jing Hao <jinghaoreg@gmail.com>
2025-12-02 13:43:07 -0500

    Initial Commit

---
+++ msdk-demo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  msdk-demo
+//
+//  Created by Jing Hao on 12/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
